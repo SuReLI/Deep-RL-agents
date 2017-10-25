c85ec49038488cef1246224f3ad76dc4a181bf73
Val95240 <Valentin.GUILLET@student.isae-supaero.fr>
2017-10-25 11:20:56 +0200

    Add Flappy Bird Environment

---
+++ Environment_Tests/FlappyBird/main.py
@@ -0,0 +1,36 @@
+
+import tensorflow as tf
+
+from Agent import Agent
+
+from Displayer import DISPLAYER
+from Saver import SAVER
+
+import parameters
+
+if __name__ == '__main__':
+    
+    tf.reset_default_graph()
+
+    with tf.Session() as sess:
+
+        sess.run(tf.global_variables_initializer())
+
+        agent = Agent(sess)
+        SAVER.set_sess(sess)
+
+        SAVER.load(agent)
+
+        try:
+            agent.run()
+        except KeyboardInterrupt:
+            pass
+        print("End of the run")
+        SAVER.save(agent.nb_ep)
+        DISPLAYER.disp()
+
+        agent.play(10)
+
+        agent.play(3, "results/gif/{}".format(parameters.ENV))
+
+    agent.stop()

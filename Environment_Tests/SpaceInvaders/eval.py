a1234c93ef644941037e1ddfdf1290f9737afbcb
Val95240 <Valentin.GUILLET@student.isae-supaero.fr>
2017-11-15 16:33:25 +0100

    Add test SpaceInvaders

---
+++ Environment_Tests/SpaceInvaders/eval.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import numpy as np
+
+from Network import Network
+from Environment import Environment
+
+from Saver import SAVER
+import settings
+
+NB_PLAY = 1
+GIF = True
+
+if __name__ == '__main__':
+
+    tf.reset_default_graph()
+
+    device = "/cpu:0"
+    stop_requested = False
+
+    print("Creating the global network...")
+    global_network = Network(0, device)
+    env = Environment(True)
+
+    # prepare session
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=False,
+                                            allow_soft_placement=True))
+
+    sess.run(tf.global_variables_initializer())
+
+    SAVER.set_sess(sess)
+    settings.LOAD = True
+    global_total_time, wall_time, total_eps, total_steps = SAVER.load()
+
+    gif_saver = []
+
+    for i in range(NB_PLAY):
+        done = False
+        reward, step = 0, 0
+
+        state = env.reset()
+        global_network.reset_state()
+
+        while not done:
+
+            pi, value = global_network.run_policy_and_value(sess, state)
+            a = np.random.choice(settings.ACTION_SIZE, p=pi)
+            state, r, done, _ = env.process(a, True)
+
+            reward += r
+            step += 1
+
+        print("Episode reward {} (in {} steps)".format(reward, step))
+        print("Saving gif...")
+        env.save_gif("results/gif/{}.gif".format(settings.ENV))
---
+++ Environment_Tests/SpaceInvaders/settings.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+ENV = "space_invaders.bin"
+ACTION_SIZE = 4
+
+DISPLAY = True
+LOAD = False
+
+NB_THREADS = 8 # parallel thread size
+
+GAMMA = 0.99
+UPDATE_FREQ = 20
+
+# RMSProp parameters
+RMSP_ALPHA = 0.99
+RMSP_EPSILON = 0.1
+
+# Learning rate distribution parameters
+INITIAL_ALPHA_LOW = 1e-4
+INITIAL_ALPHA_HIGH = 1e-2
+INITIAL_ALPHA_LOG_RATE = 0.4226 # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
+
+ENTROPY_REG = 0.01
+
+MAX_TIME_STEP = 10 * 10**7
+MAX_GRADIENT_NORM = 40.0
+
+LOG_FILE = 'summary/'
+
+
+# Display settings
+DISP_REWARD_FREQ = 5
+PLOT_FREQ = 25
+RENDER_FREQ = 50
+PERF_FREQ = 100

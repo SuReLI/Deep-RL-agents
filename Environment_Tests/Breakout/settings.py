126594ed49a052aa2b315189d0eb4066f053955f
Val95240 <Valentin.GUILLET@student.isae-supaero.fr>
2017-11-20 11:08:22 +0100

    Breakout Gif

--- Environment_Tests/Breakout/settings.py
+++ Environment_Tests/Breakout/settings.py
@@ -21,7 +21,7 @@ INITIAL_ALPHA_LOG_RATE = 0.4226 # log_uniform interpolate rate for learning rate
 
 ENTROPY_REG = 0.01
 
-MAX_TIME_STEP = 10 * 10**7
+MAX_TIME_STEP = 10 * 10**12
 MAX_GRADIENT_NORM = 40.0
 
 LOG_FILE = 'summary/'

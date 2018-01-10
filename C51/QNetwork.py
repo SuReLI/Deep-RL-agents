ff92c1651d694dbafb46c7efa2d0d0c8e57afcbc
Val95240 <Valentin.GUILLET@student.isae-supaero.fr>
2018-01-10 17:48:24 +0100

    Debugged C51

--- C51/QNetwork.py
+++ C51/QNetwork.py
@@ -28,7 +28,7 @@ class QNetwork:
 
             # Support of the distribution
             delta_z = (MAX_Q - MIN_Q) / (settings.NB_ATOMS - 1)
-            z = [MIN_Q + i * delta_z for i in range(settings.NB_ATOMS)]
+            z = tf.range(MIN_Q, MAX_Q + delta_z, delta_z)
 
             # Expected Q_value for each action in the state self.state
             self.Q_value = tf.reduce_sum(z * self.Q_distrib, axis=2)
@@ -36,10 +36,10 @@ class QNetwork:
             # Optimal action according to this Q value
             self.action = tf.argmax(self.Q_value, 1, output_type=tf.int32)
 
-            self.reward = tf.placeholder(tf.float32, [None], name='reward_ph')
-            self.not_done = tf.placeholder(tf.float32, [None], name='not_done_ph')
+            self.reward_ph = tf.placeholder(tf.float32, [None], name='reward_ph')
+            self.not_done_ph = tf.placeholder(tf.float32, [None], name='not_done_ph')
 
-            batch_size = tf.shape(self.reward)[0]
+            batch_size = tf.shape(self.reward_ph)[0]
             shape = [batch_size, settings.NB_ATOMS]
 
 
@@ -51,15 +51,28 @@ class QNetwork:
 #                     If type(bj) == int, then l == u == b                    #
 #                            NO PROJECTION !                                  #
 #                                                                             #
+#       Replace reward and not done placeholder size with [None, 1] ???       #
+#                                                                             #
+#                                                                             #
 #                                                                             #
 ###############################################################################
 
+            zz = tf.tile(z[None], [batch_size, 1])
 
-            Tz = tf.clip_by_value(self.reward + settings.DISCOUNT * self.not_done * z,
+            self.reward = self.reward_ph[:, None]
+            self.not_done = self.not_done_ph[:, None]
+
+            Tz = tf.clip_by_value(self.reward + settings.DISCOUNT * self.not_done * zz,
                                   MIN_Q, MAX_Q)
             bj = (Tz - MIN_Q) / delta_z
             l, u = tf.floor(bj), tf.ceil(bj)
-            l, u = tf.to_int32(l), tf.to_int32(u)
+            l_ind, u_ind = tf.to_int32(l), tf.to_int32(u)
+            print("*"*100)
+            print(self.reward)
+            print(self.not_done)
+            print(self.not_done * zz)
+            print(self.reward + self.not_done * zz)
+            print("*"*100)
 
             ind = tf.stack((tf.range(batch_size), self.action), axis=1)
             Q_distrib_optimal_action = tf.gather_nd(self.Q_distrib, ind)
@@ -69,8 +82,9 @@ class QNetwork:
             self.loss = tf.zeros([batch_size])
 
             for j in range(settings.NB_ATOMS):
-                l_index = tf.stack((tf.range(batch_size), l[:, j]), axis=1)
-                u_index = tf.stack((tf.range(batch_size), u[:, j]), axis=1)
+                print(tf.shape(l[:, j]))
+                l_index = tf.stack((tf.range(batch_size), l_ind[:, j]), axis=1)
+                u_index = tf.stack((tf.range(batch_size), u_ind[:, j]), axis=1)
 
                 main_Q_distrib_l = tf.gather_nd(self.main_Q_distrib, l_index)
                 main_Q_distrib_u = tf.gather_nd(self.main_Q_distrib, u_index)
@@ -100,7 +114,7 @@ class QNetwork:
             main_Q_distrib[i] = Q_distrib[i, action[i]]
 
         feed_dict = {self.state_ph: np.stack(next_state),
-                     self.reward: reward,
-                     self.not_done: not_done,
+                     self.reward_ph: reward,
+                     self.not_done_ph: not_done,
                      self.main_Q_distrib: main_Q_distrib}
         _ = self.sess.run(self.train, feed_dict=feed_dict)
